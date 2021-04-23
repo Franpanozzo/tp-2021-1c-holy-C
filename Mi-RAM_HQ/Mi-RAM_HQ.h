cdcef44c3c950f041829cd25b15f418ebdd0bb32
utnso <utnso@ubuntu-server>
2021-04-23 19:32:42 -0300

    Agrego el header y el config de MiRAM

---
+++ Mi-RAM_HQ/Mi-RAM_HQ.h
@@ -0,0 +1,27 @@
+/*
+ * Mi-RAM_HQ.h
+ *
+ *  Created on: 23 abr. 2021
+ *      Author: utnso
+ */
+
+#ifndef MI_RAM_HQ_H_
+#define MI_RAM_HQ_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <commons/log.h>
+#include <commons/string.h>
+#include <commons/config.h>
+
+void crearSocket();
+
+#endif /* MI_RAM_HQ_H_ */
