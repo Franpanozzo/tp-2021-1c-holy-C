af81529d7b160dc1cb347d3ac2553bf672164727
Guillermo Felipetti <guill388@hotmail.com>
2021-04-23 19:55:50 -0300

    Discordiador cilient update

--- Discordiador/discordiador.c
+++ Discordiador/discordiador.c
@@ -1,10 +1,36 @@
-#include <stdio.h>
+#include "discordiador.h"
 
-int main(){
+int main(int argc, char *argv[]){
 
-	printf("Primer push");
+	struct sockaddr_in direccionServidor;
+
+	t_config* ipDiscordiador = config_create("config.cfg");
+
+	char *IP = config_get_string_value(ipDiscordiador,"IP_MI_RAM_HQ");
+
+	int *PORT = config_get_int_value(ipDiscordiador,"PUERTO_MI_RAM_HQ");
+
+	direccionServidor.sin_family = AF_INET;
+	direccionServidor.sin_addr.s_addr = inet_addr(IP);
+	direccionServidor.sin_port = htons((uint16_t)PORT);
+
+	int cliente = socket(AF_INET,SOCK_STREAM,0);
+
+	if(connect(cliente,(void*) &direccionServidor,sizeof(direccionServidor)) != 0){
+		perror("No se pudo conectar");
+		return 1;
+	}
+
+	while(1){
+		char mensaje[1000];
+
+		scanf("%s",mensaje);
+
+		send(cliente,mensaje,strlen(mensaje + 1),0);
+	}
 
 	return 0;
 }
+
 
 
---
+++ Discordiador/discordiador.h
@@ -0,0 +1,10 @@
+#ifndef DISCORDIADOR_H_
+#define DISCORDIADOR_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <commons/config.h>
+
+#endif
