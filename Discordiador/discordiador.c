a6cd3bc7b37cb84acb56e6689d2a29d8f6375afa
Guillermo Felipetti <guill388@hotmail.com>
2021-04-24 22:52:11 -0300

    Discordiador depurado

--- Discordiador/discordiador.c
+++ Discordiador/discordiador.c
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2012 Sistemas Operativos - UTN FRBA. All rights reserved.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include "discordiador.h"
 
 #define MAX_BUFFER_SIZE  200
@@ -40,19 +24,15 @@ void iniciar_conexion() {
 
 	int server_sock = socket(AF_INET, SOCK_STREAM, 0);
 	int yes = 0;
+
 	setsockopt(server_sock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
-	struct sockaddr_in* localAddress = malloc(sizeof(struct sockaddr_in));
+
 	struct sockaddr_in* serverAddress = malloc(sizeof(struct sockaddr_in));
-	localAddress->sin_addr.s_addr = INADDR_ANY;
-	localAddress->sin_port = htons(PORT + 1);
-	localAddress->sin_family = AF_INET;
-	if (bind(server_sock, (struct sockaddr*) localAddress, (socklen_t) sizeof(struct sockaddr_in)) == -1) {
-		perror("bind");
-	}
 
 	serverAddress->sin_addr.s_addr = inet_addr("127.0.0.1");
 	serverAddress->sin_port = htons(PORT);
 	serverAddress->sin_family = AF_INET;
+
 	if (connect(server_sock, (struct sockaddr*) serverAddress, sizeof(struct sockaddr_in)) == -1) {
 		perror("connect");
 	}
@@ -87,7 +67,7 @@ void iniciar_conexion() {
 		printf("Ha recibido del servidor el siguiente mensaje: %s \n", buffer);
 	}
 	free(serverAddress);
-	free(localAddress);
+
 }
 
 void comunicarse() {
