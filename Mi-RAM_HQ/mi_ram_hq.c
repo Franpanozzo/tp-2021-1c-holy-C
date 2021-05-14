a339e952de75b39acff83ba73fc081f24b3caad7
falvarezscarzella <facundoivan10@gmail.com>
2021-05-14 02:21:19 -0300

    Cambios en proceso agregados a MI-RAM

--- Mi-RAM_HQ/mi_ram_hq.c
+++ Mi-RAM_HQ/mi_ram_hq.c
@@ -1,69 +1,83 @@
 #include "mi_ram_hq.h"
 
+int idPatota = 1;
+
+t_list* listaPCB = list_Create();
 
 int main(void) {
 
-	int puerto = 3222;
+    int puerto = 3222;
 
-	int serverSock = iniciarConexionDesdeServidor(puerto);
+    int serverSock = iniciarConexionDesdeServidor(puerto);
 
-	atender_tripulantes(serverSock);
+    //Abro un thread manejar las conexiones
+    pthread_t manejo_tripulante;
+    pthread_create(&manejo_tripulante, NULL, (void) atender_tripulantes, (void)serverSock);
+    pthread_join(manejo_tripulante, (void*) NULL);
 
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
 
 
 void atender_tripulantes(int serverSock) {
 
-	while(1){
+    while(1){
 
-	int tripulanteSock = esperar_tripulante(serverSock);
+    int tripulanteSock = esperar_tripulante(serverSock);
 
-	manejar_tripulante(tripulanteSock);
+    pthread_t t;
+    pthread_create(&t, NULL, (void) manejar_tripulante, (void) tripulanteSock);
+    pthread_detach(t);
 
-
-	}
+    }
 
 }
 
 
 int esperar_tripulante(int serverSock) {
 
-	struct sockaddr_in* serverAddress = malloc(sizeof(struct sockaddr_in));
-	unsigned int len = sizeof(struct sockaddr);
+    struct sockaddr_in serverAddress = malloc(sizeof(struct sockaddr_in));
+    unsigned int len = sizeof(struct sockaddr);
 
-	int socket_tripulante = accept(serverSock, (struct sockaddr*) serverAddress, &len);
+    int socket_tripulante = accept(serverSock, (struct sockaddr) serverAddress, &len);
 
-	//log_info(logger, "Se conecto un cliente!");
+    //log_info(logger, "Se conecto un cliente!");
 
-	return socket_tripulante;
+    return socket_tripulante;
 
 }
 
 
-void manejar_tripulante(int tripulanteSock) {
+void manejar_tripulante(int tripulanteSock) { // Esta funcion deberia usar la funcion deserializarSegun() de bibliotecas
 
-	t_paquete* paquete = recibirPaquete(tripulanteSock);
-
-
-	switch(paquete->codigo_operacion){
-
-				case PERSONA:
-							deserializarPersona(paquete->buffer);
-							break;
+    t_paquete paquete = recibirPaquete(tripulanteSock);
 
 
-				default:
-						printf("\n No se puede deserializar ese tipo de estructura negro \n");
-						exit(1);
-			}
+    switch(paquete->codigo_operacion){
 
-	eliminarPaquete(paquete);
+                case PERSONA:
+                            deserializarPersona(paquete->buffer);
+                            break;
+
+                case TAREA_PATOTA:
+                {
+                            pcb* nuevoPCB = malloc(sizeof(pcb));
+                            nuevoPCB->pid = idPatota;
+                            idPatota++;
+                            nuevoPCB->listaTareas = list_Create();
+
+                            deserializarTarea(paquete->buffer, nuevoPCB->listaTareas);
+                            list_add(listaPCB, (void*) nuevoPCB);
+                }
+
+                default:
+                        printf("\n No se puede deserializar ese tipo de estructura negro \n");
+                        exit(1);
+            }
+
+    eliminarPaquete(paquete);
 }
-
-
-
 
 
 
--- Mi-RAM_HQ/mi_ram_hq.h
+++ Mi-RAM_HQ/mi_ram_hq.h
@@ -1,5 +1,5 @@
-#ifndef MI_RAM_HQ_H_
-#define MI_RAM_HQ_H_
+#ifndef MI_RAM_HQH
+#define MI_RAM_HQH
 
 
 #include <stdio.h>
@@ -14,6 +14,33 @@
 #include <string.h>
 #include <commons/config.h>
 #include <bibliotecas.h>
+
+typedef enum {
+
+    GENERAR_OXIGENO
+
+} tipoTarea;
+
+
+typedef struct {
+
+    tipoTarea tarea;
+    uint32_t parametro;
+    uint32_t posX;
+    uint32_t posY;
+    uint32_t tiempo;
+
+} t_tarea;
+
+
+typedef enum {
+
+    uint32_t pid;
+    t_list* listaTareas;
+
+} pcb;
+
+
 
 void atender_tripulantes(int serverSock);
 int esperar_tripulante(int serverSock);
