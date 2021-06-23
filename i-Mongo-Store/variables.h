fd5fc7f9403a457dc3ad8f9b538c4ea175856b9f
juanfsc <juanfrancisco14sc@gmail.com>
2021-06-23 02:00:48 -0300

    chau variables.h

--- i-Mongo-Store/variables.h
+++ i-Mongo-Store/variables.h
@@ -2,47 +2,6 @@
 #define VARIABLES_H_
 
 
-#include "i_mongo_store.h"
-
-
-typedef struct{
-
-	uint32_t block_size;
-	uint32_t blocks;
-	t_bitarray* bitmap;
-
-}t_superBloque;
-
-typedef struct{
-
-	uint32_t tamanioArchivo;
-	uint32_t cantidadBloques;
-	t_list* bloquesQueOcupa;
-	char caracterLlenado;
-	char* md5_archivo;
-
-}t_file;
-
-typedef struct{
-
-	char* puntoMontaje;
-	int puerto;
-	int tiempoSincronizacion;
-	char* posicionesSabotaje;
-
-}t_datosConfig;
-
-
-t_config* config;
-t_log* logImongo;
-char** todasLasTareasIO;
-t_datosConfig* datosConfig;
-t_superBloque* superBloque;
-char* bitArray;
-t_file* oxigeno;
-t_file* comida;
-t_file* basura;
-
 
 #endif
 
