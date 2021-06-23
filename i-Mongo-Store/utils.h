ccd90c654e595b8d88705d4f4160ccb8d67afddb
juanfsc <juanfrancisco14sc@gmail.com>
2021-06-23 02:04:35 -0300

    vuelve variables.h

--- i-Mongo-Store/utils.h
+++ i-Mongo-Store/utils.h
@@ -1,47 +1,10 @@
-#include "variables.h"
-#include "i_mongo_store.h"
-
 
 #ifndef UTILS_H_
 #define UTILS_H_
 
-typedef struct{
+#include "variables.h"
+#include "i_mongo_store.h"
 
-	uint32_t block_size;
-	uint32_t blocks;
-	t_bitarray* bitmap;
-
-}t_superBloque;
-
-typedef struct{
-
-	uint32_t tamanioArchivo;
-	uint32_t cantidadBloques;
-	t_list* bloquesQueOcupa;
-	char caracterLlenado;
-	char* md5_archivo;
-
-}t_file;
-
-typedef struct{
-
-	char* puntoMontaje;
-	int puerto;
-	int tiempoSincronizacion;
-	char* posicionesSabotaje;
-
-}t_datosConfig;
-
-
-t_config* config;
-t_log* logImongo;
-char** todasLasTareasIO;
-t_datosConfig* datosConfig;
-t_superBloque* superBloque;
-char* bitArray;
-t_file* oxigeno;
-t_file* comida;
-t_file* basura;
 
 void crearConfig();
 char * pathLog();
