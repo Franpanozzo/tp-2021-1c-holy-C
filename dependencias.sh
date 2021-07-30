727cb6657b3180866fc9038a30c918a5253554ae
falvarezscarzella <62475073+falvarezscarzella@users.noreply.github.com>
2021-07-30 17:32:39 -0300

    Agregadas dependencias mapa

--- dependencias.sh
+++ dependencias.sh
@@ -8,3 +8,15 @@ sudo make install
 echo "clonando a-mongos-pruebas"
 cd ~
 git clone https://github.com/sisoputnfrba/a-mongos-pruebas.git
+
+echo "instalando ncurses"
+cd ~
+sudo apt-get install libncurses5-dev
+
+echo "clonando nivel-gui"
+cd ~
+git clone https://github.com/sisoputnfrba/so-nivel-gui-library.git
+cd ~/so-nivel-gui-library
+sudo make install
+
+
